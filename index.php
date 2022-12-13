d6c041291eaecf6a5bd07a17c79e7aec0a3ce19d
Roman Stolyarov <nokerka@ya.ru>
2022-12-13 10:30:00 +0300

    Change contact information

--- index.php
+++ index.php
@@ -3,11 +3,7 @@
 	<head>
 	<style>
 	.gm-style-moc{background-color:rgba(0,0,0,.45);pointer-events:none;text-align:center;transition:opacity ease-in-out}.gm-style-mot{color:white;font-family:Roboto,Arial,sans-serif;font-size:22px;margin:0;position:relative;top:50%;transform:translateY(-50%);-webkit-transform:translateY(-50%);-ms-transform:translateY(-50%)}
-	</style>
-	<style>
 	.gm-style img{max-width: none;}.gm-style {font: 400 11px Roboto, Arial, sans-serif; text-decoration: none;}
-	</style>
-	<style>
 	.wf-force-outline-none[tabindex="-1"]:focus{outline:none;}
 	</style>
 	<meta charset="utf-8">
@@ -23,6 +19,13 @@
 	<script type="text/javascript">
 	WebFont.load({  google: {    families: ["Open Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic"]  }});
 	</script>
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<link rel="manifest" href="/site.webmanifest">
+	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#004ae2">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="theme-color" content="#ffffff">
 	<!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif]-->
 	<script type="text/javascript">
 	!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);
@@ -788,7 +791,7 @@
 		<div class="blog-wrapper">
 			<div class="blog-section-title" style="opacity: 1; transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;">
 				<h3 class="text-center">Стоимость <strong>объекта</strong><br></h3>
-				<h2 class="text-center"><strong style="color:#004ae2;">18 200 000 ₽</strong><br></h2>
+				<h2 class="text-center"><strong style="color:#004ae2;">12 000 000 ₽</strong><br></h2>
 				<p class="gray-paragraph text-centered" style="max-width:480px;margin:0 auto;">Будте уверены, стоимость этого дома полностью потрачена на его строительство, материалы выбирались самым строгим образом, для себя...<br></p>
 			</div>
 		</div>
